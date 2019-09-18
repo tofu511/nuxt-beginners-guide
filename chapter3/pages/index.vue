2e48aeac77ca302c90521e9a8e02e31af4466f9e
tofu511 <mokmok511@gmail.com>
2019-09-18 17:58:53 +0900

    finish chapter3

---
+++ chapter3/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Index page</h2>
+    <ul>
+      <li>
+        <nuxt-link to="/login">ログインページへ</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/authed-route">認証が必要なページ</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
