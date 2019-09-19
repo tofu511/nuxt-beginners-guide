1297c8be202fe68bed3793756dae30024064212e
tofu511 <mokmok511@gmail.com>
2019-09-19 23:08:30 +0900

    implement like

--- nuxt-blog/app/pages/posts/_id.vue
+++ nuxt-blog/app/pages/posts/_id.vue
@@ -7,7 +7,19 @@
           <small>by {{post.user.id}}</small>
         </div>
         <p>{{post.body}}</p>
-        <p class="text-right">{{post.created_at | time}}</p>
+        <p class="text-right">
+          <el-button :disabled="!idLoggedIn" type="warning" v-if="isLiked" @click="unlike" round>
+             <span class="el-icon-star-on"></span>
+             <span>{{post.likes.length}}</span>
+          </el-button>
+          <el-button :disabled="!isLoggedIn" type="warning" v-else @click="like" round>
+            <span class="el-icon-star-off"></span>
+            <span>{{post.likes.length}}</span>
+          </el-button>
+        </p>
+        <p class="text-right">
+          {{post.created_at | time}}
+        </p>
       </el-card>
       <p>
         <nuxt-link to="/posts">&lt; 投稿一覧へ戻る</nuxt-link>
@@ -19,6 +31,7 @@
 <script>
 import moment from '~/plugins/moment'
 import { mapGetters, mapActions } from 'vuex'
+import cloneDeep from 'lodash.clonedeep'
 
 export default {
   async asyncData({ store, route, error }) {
@@ -39,7 +52,32 @@ export default {
     post() {
       return this.posts.find(p => p.id === this.$route.params.id)
     },
+    isLiked() {
+      if (!this.user) {
+        return false
+      }
+      return this.post.likes.find(l => l.user_id === this.user.id)
+    },
+    ...mapGetters(['user', 'isLoggedIn']),
     ...mapGetters('posts', ['posts'])
+  },
+  methods: {
+    like() {
+      if (!this.isLoggedIn) {
+        return
+      }
+      const likePayload = { user: this.user, post: this.post }
+      this.addLikeToPost(cloneDeep(likePayload))
+      this.addLikeLogToUser(cloneDeep(likePayload))
+    },
+    unLike() {
+      if (!this.isLoggedIn) {
+        return
+      }
+      // TODO いいね取り消し処理
+    },
+    ...mapActions(['addLikeLogToUser']),
+    ...mapActions('posts', ['addLikeToPost'])
   },
   filters: {
     time(val) {
